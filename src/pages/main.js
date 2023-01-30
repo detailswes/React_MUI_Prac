7446c90c8879399d8d6b1d20b08b95918faf3aae
details.wes <details.wes@gmail.com>
2023-01-30 19:04:23 +0530

    data table and popup added

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,10 +1,78 @@
 import { Button, Divider, FormControl, Grid, InputLabel, MenuItem, Select, TextField, ThemeProvider } from '@mui/material';
+import Stack from '@mui/material/Stack';
 import React, { useState } from 'react';
+import { styled } from '@mui/material/styles';
 import {theme} from "../utils/theme.js";
 import Typography from '@mui/material/Typography';
-import { Box } from '@mui/system';
-import { DataGrid } from '@mui/x-data-grid';
-import { useDemoData } from '@mui/x-data-grid-generator';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import EditIcon from '@mui/icons-material/Edit';
+import LinkIcon from '@mui/icons-material/Link';
+import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';
+import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
+import RadioButtonCheckedIcon from '@mui/icons-material/RadioButtonChecked';
+import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
+import ImageIcon from '@mui/icons-material/Image';
+import NoteAddIcon from '@mui/icons-material/NoteAdd';
+// popup imports
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+
+// table code stat from here
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.primary.main,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
+
+function createData(id, title, desc, status, action) {
+  return { id, title, desc, status, action };
+}
+
+const rows = [
+  createData('1', 'MUI + REACT LEARING (0)', 'PLAN OF ACTION- STUDY MUI FOR...', 'Open', <><EditIcon fontSize='small' sx={{marginLeft: 1}} /></>),
+  createData('2', 'MUI + REACT LEARING (0)', 'PLAN OF ACTION- STUDY MUI FOR...', 'Open', <><EditIcon fontSize='small' sx={{marginLeft: 1}} /></>),
+  createData('3', 'MUI + REACT LEARING (0)', 'PLAN OF ACTION- STUDY MUI FOR...', 'Open', <><EditIcon fontSize='small' sx={{marginLeft: 1}} /></>),
+  createData('4', 'MUI + REACT LEARING (0)', 'PLAN OF ACTION- STUDY MUI FOR...', 'Open', <><EditIcon fontSize='small' sx={{marginLeft: 1}} /></>),
+  createData('5', 'MUI + REACT LEARING (0)', 'PLAN OF ACTION- STUDY MUI FOR...', 'Open', <><EditIcon fontSize='small' sx={{marginLeft: 1}} /></>),
+];
+// table code ends here
+
+// popup code start from here
+const style = {
+    position: 'absolute',
+    top: '50%',
+    right: '-20%',
+    transform: 'translate(-50%, -50%)',
+    boxShadow: 'none',
+    width: 700,
+    height: '94vh',
+    bgcolor: 'background.paper',
+    border: '2px solid #fff',
+    p: 4,
+  };
 
 const Main = () => {
 
@@ -19,12 +87,10 @@ const Main = () => {
         setProjectstatus(e.target.value)
     }
 
-    // table functions
-    const { data } = useDemoData({
-        dataSet: 'Commodity',
-        rowLength: 5,
-        maxColumns: 6,
-    });
+    // popup onclick states
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
 
   return (
     <>
@@ -83,16 +149,130 @@ const Main = () => {
             </Grid>
 
             <Grid mt={5} borderRadius={2} boxShadow={4} p={4}>
-            <div style={{ height: 400, width: '100%' }}>
-            <div style={{ display: 'flex', height: '100%' }}>
-                <div style={{ flexGrow: 1 }}>
-                <DataGrid {...data} />
-                </div>
-            </div>
-            </div>
+            <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 700 }} aria-label="customized table">
+                <TableHead>
+                <TableRow>
+                    <StyledTableCell>ID</StyledTableCell>
+                    <StyledTableCell align="left">Title</StyledTableCell>
+                    <StyledTableCell align="left">Description</StyledTableCell>
+                    <StyledTableCell align="left">Status</StyledTableCell>
+                    <StyledTableCell align="left">Action</StyledTableCell>
+                </TableRow>
+                </TableHead>
+                <TableBody>
+                {rows.map((row, index) => (
+                    <StyledTableRow key={index}>
+                    <StyledTableCell component="th" scope="row">
+                        {row.id}
+                    </StyledTableCell>
+                    <StyledTableCell align="left">{row.title}</StyledTableCell>
+                    <StyledTableCell align="left">{row.desc}</StyledTableCell>
+                    <StyledTableCell align="left">{row.status}</StyledTableCell>
+                    <StyledTableCell align="left"><VisibilityIcon onClick={handleOpen} fontSize='small'/> {row.action}</StyledTableCell>
+                    </StyledTableRow>
+                ))}
+                </TableBody>
+            </Table>
+            </TableContainer>
+
+            {/* table view project model */}
+            <Modal
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                open={open}
+                onClose={handleClose}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                timeout: 500,
+                }}
+            >
+                <Fade in={open}>
+                <Box p={1} sx={style}>
+                    <Grid container>
+                        <Grid md="8">
+                            <Typography variant='p'>Project / Fotunet - Valuation / FV-321 <LinkIcon fontSize='small' pt={2} /></Typography>
+                        </Grid>
+                        <Grid md="4" textAlign='right'>
+                            <ArrowCircleLeftIcon/> <ArrowCircleRightIcon/>
+                        </Grid>
+
+                        <Grid md={12} mt={5}>
+                            <Typography sx={{fontSize: '20px', fontWeight: '700'}}>Get the Feedback on Clutch</Typography>
+                        </Grid>
+
+                        <Grid md={12} mt={2}>
+                        <Stack spacing={2} direction="row">
+                            <Button sx={{backgroundColor: '#EBF2FF', color: '#505F79', fontSize: '13px', padding: '10px 20px'}}variant="text">5 Attachments</Button>
+                            <Button variant="contained">Contained</Button>
+                            <Button variant="outlined">Outlined</Button>
+                        </Stack>
+                        </Grid>
+
+                        <Grid md={12} mt={2}>
+                            <Typography pb={2} varient="p">We're good to proceed on this task and would expect to complete it today.
+                            Here's the further instruction client has shared. Find the attached below
+                            One important thing, I want to share our marketing language with you so the design will be according to it.
+                            </Typography>
+                            <Divider/>
+                        </Grid>
+
+                        <Grid md={12} mt={5}>
+                            <Box mb={2} sx={{display: 'flex', justifyContent: 'start', alignItems: 'center'}}>
+                                <Box width="4%"><RadioButtonCheckedIcon fontSize='small' pt={2} sx={{color: '#F3B20A'}}/></Box>
+                                <Box width='30%'><Typography varient="p">Category Name</Typography></Box>
+                                <Box width='66%'><Typography varient="p" color="#005DFF">Design</Typography></Box>
+                            </Box>
+                            <Box mb={2} sx={{display: 'flex', justifyContent: 'start', alignItems: 'center'}}>
+                                <Box width="4%"><RadioButtonCheckedIcon fontSize='small' pt={2} sx={{color: '#01875BB5'}}/></Box>
+                                <Box width='30%'><Typography varient="p">Assigned by</Typography></Box>
+                                <Box width='66%'><Typography varient="p" color="#005DFF">Harsh Vardhan</Typography></Box>
+                            </Box>
+                            <Box mb={2} sx={{display: 'flex', justifyContent: 'start', alignItems: 'center'}}>
+                                <Box width="4%"><RadioButtonCheckedIcon fontSize='small' pt={2} sx={{color: '#005DFFB2'}}/></Box>
+                                <Box width='30%'><Typography varient="p">Assigned To</Typography></Box>
+                                <Box width='66%'><Typography varient="p" color="#005DFF">Kamal Kant, Shalini Katoch, Sanjay Midha.</Typography></Box>
+                            </Box>
+                            <Box pb={4} mb={2} sx={{display: 'flex', justifyContent: 'start', alignItems: 'center'}}>
+                                <Box width="4%"><RadioButtonCheckedIcon fontSize='small' pt={2} sx={{color: '#E82121BD'}}/></Box>
+                                <Box width='30%'><Typography varient="p">Deadline</Typography></Box>
+                                <Box width='66%'><Typography varient="p" color="#005DFF">25 Dec, 2022</Typography></Box>
+                            </Box>
+                            <Divider/>
+                        </Grid>
+
+                        <Grid md={12} mt={5}>
+                            <Box display='flex' justifyContent='start'>
+                                <Box mr={3} pb={4} mb={2} sx={{display: 'flex', alignItems: 'center'}}>
+                                    <Box mr={1}><LinkIcon fontSize='small'/></Box>
+                                    <Box><Typography varient="p" color="#005DFF">Files.zip</Typography></Box>
+                                </Box>
+                                <Box mr={3} pb={4} mb={2} sx={{display: 'flex', justifyContent: 'start', alignItems: 'center'}}>
+                                    <Box mr={1}><PictureAsPdfIcon fontSize='small' sx={{color: '#EB0000'}} /></Box>
+                                    <Box><Typography varient="p" color="#005DFF">Fortunet.pdf</Typography></Box>
+                                </Box>
+                                <Box mr={3} pb={4} mb={2} sx={{display: 'flex', justifyContent: 'start', alignItems: 'center'}}>
+                                    <Box mr={1}><ImageIcon fontSize='small' sx={{color: '#666666'}} /></Box>
+                                    <Box><Typography varient="p" color="#005DFF">Image.jpg</Typography></Box>
+                                </Box>
+                                <Box mr={3} pb={4} mb={2} sx={{display: 'flex', justifyContent: 'start', alignItems: 'center'}}>
+                                    <Box mr={1}><NoteAddIcon fontSize='small' sx={{color: '#005DFF'}} /></Box>
+                                    <Box><Typography varient="p" color="#005DFF">Excel.xls</Typography></Box>
+                                </Box>
+                            </Box>
+                        </Grid>
+
+                    </Grid>
+                </Box>
+                </Fade>
+            </Modal>
+
             </Grid>
 
         </Grid>
+
+        
         
       </ThemeProvider>
     </>
